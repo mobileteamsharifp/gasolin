5dfb7e6c3e7961bc003a00352aa55e64a787897c
Mostafa <mostafa.ghadimi@yahoo.com>
2019-03-04 23:47:07 +0330

    Buttons added and Layout created

--- app/src/main/java/com/example/phase1/MainActivity.java
+++ app/src/main/java/com/example/phase1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.phase1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getButton(View view) {
+
+    }
+
+    public void refreshButton(View view) {
+
+    }
+
+    public void clearButton (View view) {
+
     }
 }
